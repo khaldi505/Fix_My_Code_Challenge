ff7f053cb472d295325d472d37666d97c1727df6
khaldi505 <habetlbrika69@gmail.com>
2020-04-04 16:57:58 +0200

    1-print_square.js

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -13,8 +13,10 @@ if (process.argv.length <= 2) {
     process.stderr.write("Example: ./1-print_square.js 8\n");
     process.exit(1)
 }
-
-size = parseInt(process.argv[2], process.argv[2])
+/**
+ * fixing The parseInt() function , radix is 10 (decimal) not 0x "16"
+ */
+size = parseInt(process.argv[2], 10)
 
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
